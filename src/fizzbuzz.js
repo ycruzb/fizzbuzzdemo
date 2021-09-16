2bcab4bd169405a0bd6cf33d64b0a29fdd17c886
ycruzb <ycruzb@gmail.com>
2021-09-15 21:13:35 -0400

    feat: add logic to the fizzbuzz function in order to pass all tests

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,3 +1,17 @@
 module.exports.fizzbuzzFunction = function () {
-  return [];
+  var result = [];
+  for (let i = 1; i <= 100; i++) {
+    let isDividibleBy3 = i % 3 === 0;
+    let isDivisibleBy5 = i % 5 === 0;
+    if (isDividibleBy3 && isDivisibleBy5) {
+      result.push("FizzBuzz");
+    } else if (isDividibleBy3) {
+      result.push("Fizz");
+    } else if (isDivisibleBy5) {
+      result.push("Buzz");
+    } else {
+      result.push(i);
+    }
+  }
+  return result;
 };
